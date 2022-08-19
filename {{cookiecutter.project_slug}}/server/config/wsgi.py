56442357aedeccb1283fd9822073ed802d81baa1
Nomadicode <dev@nomadicode.com>
2022-08-19 00:22:41 -0400

    Fixing template

--- {{cookiecutter.project_slug}}/server/config/wsgi.py
+++ {{cookiecutter.project_slug}}/server/config/wsgi.py
@@ -16,6 +16,4 @@ from django.core.wsgi import get_wsgi_application
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(ROOT_DIR / "apps"))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
-
 application = get_wsgi_application()
